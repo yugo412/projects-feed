f52759507116956d0d40b72292db94aeb7f84904
Yugo <dedy.yugo.purwanto@gmail.com>
2023-09-23 11:54:05 +0700

    init db using SQLite

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,19 +6,55 @@ import (
 	"os"
 	"projects-feed/cmd/api/router"
 	"projects-feed/cron"
+	"projects-feed/models"
 
 	"github.com/gookit/slog"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
+// Init database connection, by default it uses SQLite
 func init() {
-	cron.Run()
+	var err error
+	db, err = gorm.Open(sqlite.Open("db.sqlite"), &gorm.Config{
+		//Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	env := os.Getenv("ENV")
+
+	// auto migrate for non prod env
+	if env != "production" {
+		err = db.AutoMigrate(
+			&models.Author{},
+			&models.Vendor{},
+			&models.Project{},
+		)
+		if err != nil {
+			slog.Errorf("Failed to auto-migrate database: %v", err)
+		}
+	}
+}
+
+// Run every job that registered in cron/schedulers
+func init() {
+	cron.Run(cron.Cron{
+		DB: db,
+	})
 }
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-		os.Setenv("PORT", port)
+		err := os.Setenv("PORT", port)
+		if err != nil {
+			slog.Errorf("Failed to set port %s: %v", port, err)
+		}
 	}
 
 	env := os.Getenv("ENV")
