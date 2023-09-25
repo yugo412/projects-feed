6e3d0c89c17e575c1ac74edc43f8de94660126bc
Yugo <dedy.yugo.purwanto@gmail.com>
2023-09-25 18:32:25 +0700

    enable db and cron

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,43 +5,47 @@ import (
 	"net/http"
 	"os"
 	"projects-feed/cmd/api/router"
+	"projects-feed/cron"
+	"projects-feed/models"
 
 	"github.com/gookit/slog"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-//var db *gorm.DB
-//
-//// Init database connection, by default it uses SQLite
-//func init() {
-//	var err error
-//	db, err = gorm.Open(sqlite.Open("db.sqlite"), &gorm.Config{
-//		//Logger: logger.Default.LogMode(logger.Info),
-//	})
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	env := os.Getenv("ENV")
-//
-//	// auto migrate for non prod env
-//	if env != "production" {
-//		err = db.AutoMigrate(
-//			&models.Author{},
-//			&models.Vendor{},
-//			&models.Project{},
-//		)
-//		if err != nil {
-//			slog.Errorf("Failed to auto-migrate database: %v", err)
-//		}
-//	}
-//}
-//
-//// Run every job that registered in cron/schedulers
-//func init() {
-//	cron.Run(cron.Cron{
-//		DB: db,
-//	})
-//}
+var db *gorm.DB
+
+// Init database connection, by default it uses SQLite
+func init() {
+	var err error
+	db, err = gorm.Open(sqlite.Open("db.sqlite"), &gorm.Config{
+		//Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	env := os.Getenv("ENV")
+
+	// auto migrate for non prod env
+	if env != "production" {
+		err = db.AutoMigrate(
+			&models.Author{},
+			&models.Vendor{},
+			&models.Project{},
+		)
+		if err != nil {
+			slog.Errorf("Failed to auto-migrate database: %v", err)
+		}
+	}
+}
+
+// Run every job that registered in cron/schedulers
+func init() {
+	cron.Run(cron.Cron{
+		DB: db,
+	})
+}
 
 func main() {
 	port := os.Getenv("PORT")
