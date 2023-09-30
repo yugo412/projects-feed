6c7c75f24020afdf8bd86d08d31106d2336513c9
Yugo <dedy.yugo.purwanto@gmail.com>
2023-09-30 10:06:34 +0700

    disable fetch project and store it to db

--- cron/cron.go
+++ cron/cron.go
@@ -3,31 +3,14 @@ package cron
 import (
 	"github.com/gookit/slog"
 	"github.com/robfig/cron/v3"
-
-	"gorm.io/gorm"
 )
 
-type Cron struct {
-	DB *gorm.DB
-}
-
-func Run(e Cron) {
+func Run() {
 	var err error
-
 	c := cron.New()
 
-	_, err = c.AddFunc("@every 5m", func() {
-		err := FetchProjects(e.DB)
-		if err != nil {
-			slog.Errorf("Failed to fetch projects: %v", err)
-		}
-	})
-	if err != nil {
-		slog.Errorf("Failed to run cron: %v", err)
-	}
-
 	_, err = c.AddFunc("@every 1m", func() {
-		err := UpdateProject(e.DB)
+		err := UpdateProject()
 		if err != nil {
 			slog.Errorf("Failed to update existing projects: %v", err)
 		}
