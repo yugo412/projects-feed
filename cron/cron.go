e51efc8c43888b8a9e78d1c49b8689fecd5ab239
Yugo <dedy.yugo.purwanto@gmail.com>
2023-10-02 13:33:34 +0700

    change cron period

--- cron/cron.go
+++ cron/cron.go
@@ -9,7 +9,10 @@ func Run() {
 	var err error
 	c := cron.New()
 
-	_, err = c.AddFunc("@every 10s", func() {
+	_, err = c.AddFunc("@every 1m", func() {
+		// by default, it will try to fetch projects every minute
+		// but, since the projects is stored to the cache for 10 minutes
+		// the projects won't re-fetch from the source until it expires
 		err := PrefectProject()
 		if err != nil {
 			slog.Errorf("Failed to fetch projects: %v", err)
@@ -19,7 +22,9 @@ func Run() {
 		slog.Errorf("Failed to run cron: %v", err)
 	}
 
-	_, err = c.AddFunc("@every 1m", func() {
+	_, err = c.AddFunc("@every 30s", func() {
+		// same behavior with prefetch-project
+		// only fetch project detail from source if there are no additional infos available
 		err := UpdateProject()
 		if err != nil {
 			slog.Errorf("Failed to update existing projects: %v", err)
